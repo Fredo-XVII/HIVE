5f014b3bec242530d2fc14491f1673c1d777a475
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2017-05-08 15:06:46 -0500

    Create Date_Plus_Minus.sql

---
+++ Date_Plus_Minus.sql
@@ -0,0 +1,4 @@
+# This code does plus or minus from a date 
+
+date1 =	date_add(string startdate, int days)	Add a number of days to startdate: date_add(‘2008-12-31’, 1) = ‘2009-01-01’
+date2 =	date_sub(string startdate, int days)  Subtract a number of days to startdate: date_sub(‘2008-12-31’, 1) = ‘2008-12-30’
