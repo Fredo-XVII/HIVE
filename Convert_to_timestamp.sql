5e37cc7b471190157584880aecbe8d6265f21ae7
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2018-06-28 09:59:50 -0500

    Update Convert_to_timestamp.sql

--- Convert_to_timestamp.sql
+++ Convert_to_timestamp.sql
@@ -1,3 +1,3 @@
 select 
-substr(from_unixtime(unix_timestamp(concat(substr(a.created_ts,0,10),' ', substr(a.created_ts,12,5),':0000'))),0,10) as timestamp
+min(substr(from_unixtime(unix_timestamp(concat(substr(a.created_ts,0,10),' ', substr(a.created_ts,12,5),':0000'))),0,22)) AS time_test
 from table
